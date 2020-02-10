5a023659e6e8c053ff08f316b19c1c6086925cb7
Liene Verzemnieks <liene@circleci.com>
2020-02-10 09:06:01 -0800

    it's my own code, and now it's also refactored

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -13,21 +13,24 @@ func TestHello(t *testing.T) {
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
-
+	assertCorrectMessage := func(t *test.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Liene")
+		want := "Hello, Liene"
+
+		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *t.testing.T) {
+	t.Run("empty string defaults to 'World'", func(t *t.testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
