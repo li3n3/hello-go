159496c2aeef9c4c9a8c5df071add1af5cc6b6ab
Liene Verzemnieks <liene@circleci.com>
2020-02-10 09:18:45 -0800

    what if instead of a test with typos we had a good test. rm dup test, too

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,18 +2,18 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
+/* func TestHello(t *testing.T) {
 	got := Hello("Liene")
 	want := "Hello, Liene"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
-}
+} */
 
 func TestHello(t *testing.T) {
 
-	assertCorrectMessage := func(t *test.T, got, want string) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -23,14 +23,12 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Liene")
 		want := "Hello, Liene"
-
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("empty string defaults to 'World'", func(t *t.testing.T) {
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
 		assertCorrectMessage(t, got, want)
 	})
 }
